f935d9e9479d8284f8135644f8c51c5607ca03af
Patao <imaimizuki150205@yahoo.co.jp>
2021-07-27 20:38:57 +0900

    refactoring

--- src/components/settlement/ConfirmationCard.tsx
+++ src/components/settlement/ConfirmationCard.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import style from "./confirmationCard.module.scss";
+import style from "./ConfirmationCard.module.scss";
 
 type Props = {
   product: {
