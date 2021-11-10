62f6ef924a9ef0c2cc9dfeb4aa9c9ce2bb613867
Patao150205 <imaimizuki150205@yahoo.co.jp>
2021-11-10 09:03:54 +0900

    edit BaseUrl.js

--- src/utils/BaseUrl.js
+++ src/utils/BaseUrl.js
@@ -1,4 +1,9 @@
-const BaseUrl =
-  process.env.NODE_ENV !== 'production' ? `http://localhost:3000` : `https://plush-toy-shop.patapatao.com`;
+let BaseUrl = '';
 
+if (process.env.NODE_ENV !== 'production') {
+  BaseUrl = `http://localhost:3000`
+} else {
+  // リバースプロキシを使用しているため
+   BaseUrl = (process.title === 'browser') ? `https://plush-toy-shop.patapatao.com`:  `http://localhost:${process.env.PORT}`; 
+}
 module.exports = BaseUrl;
